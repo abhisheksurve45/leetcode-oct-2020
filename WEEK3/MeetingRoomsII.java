93330b7bf65455001b108dc1a2c663a2b96266e2
Abhishek Surve <abhishek.sur@media.net>
2020-10-15 22:37:05 +0530

    W3Q1: MeetingRoomsII W3Q1: RotateArray

--- WEEK3/MeetingRoomsII.java
+++ WEEK3/MeetingRoomsII.java
@@ -1,3 +1,15 @@
 public class MeetingRoomsII {
 
+    public int minMeetingRooms(Interval[] intervals) {
+        if (intervals.length == 0) return 0;
+        Arrays.sort(intervals, (a, b) -> a.start - b.start);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+        minHeap.add(intervals[0].end);
+        for (int i = 1; i < intervals.length; ++i) {
+            if (minHeap.peek() <= intervals[i].start) minHeap.poll();
+            minHeap.add(intervals[i].end);
+        }
+
+        return minHeap.size();
+    }
 }
